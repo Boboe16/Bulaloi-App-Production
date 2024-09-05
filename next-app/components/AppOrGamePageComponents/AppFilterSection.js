6866a22e338af8523f6d2ce3252d45fe4395f2a6
Boboe16 <jerixmodz@gmail.com>
2024-09-06 05:40:31 +0800

    taeee

--- next-app/components/AppOrGamePageComponents/AppFilterSection.js
+++ next-app/components/AppOrGamePageComponents/AppFilterSection.js
@@ -4,28 +4,28 @@ function AppFilterSection({ id, mostDownloads, newest, hot }) {
   return (
     <div id={`${id}-filter`} className="row App-Game-Section">
       <div className="col-2 d-flex align-items-center">
-        <h6 className="text-center mx-auto">{id}</h6>
+        <h4 className="text-center mx-auto">{id}</h4>
       </div>
       <div className="col-3 d-flex align-items-center">
-        <h6 className="text-center mx-auto">Sort by:</h6>
+        <h4 className="text-center mx-auto">Sort by:</h4>
       </div>
       <div className="col-3 d-flex align-items-center">
-        <h6 id="Downloads" className="text-center mx-auto"
+        <h4 id="Downloads" className="text-center mx-auto"
         onClick={() => mostDownloads(id)}>
           Downloads
-        </h6>
+        </h4>
       </div>
       <div className="col-2 d-flex align-items-center">
-        <h6 id="Newest" className="text-center mx-auto"
+        <h4 id="Newest" className="text-center mx-auto"
         onClick={() => newest(id)}>
           Newest
-        </h6>
+        </h4>
       </div>
       <div className="col-2 d-flex align-items-center">
-        <h6 id="Hot" className="text-center mx-auto"
+        <h4 id="Hot" className="text-center mx-auto"
         onClick={() => hot(id)}>
           Hot
-        </h6>
+        </h4>
       </div>
     </div>
   );
--- next-app/components/GlobalComponents/Footer.js
+++ next-app/components/GlobalComponents/Footer.js
@@ -1,11 +1,8 @@
 export default function Footer() {
     return (
         <footer className="row">
-            <p>�� 2021 Bulaloi. All rights reserved.</p>
+            <p>�� 2024 Bulaloi. All rights reserved.</p>
             <p>techiebulaloi@gmail.com</p>
-            {/* <div>
-                <button type="button" class="btn btn-light">Contact us</button>
-            </div> */}
         </footer>
     )
 }
